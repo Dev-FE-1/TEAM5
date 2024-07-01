35b0dfcb268be5806ff0f22d021dbcc2ccd73611
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-07-02 00:23:24 +0900

    admin/commute 페이지 초안 완성

--- src/main.js
+++ src/main.js
@@ -24,25 +24,28 @@ const init = () => {
   window.addEventListener("popstate", router);
 };
 
-function router() {
-  const routes = {
-    "/": Home,
-  
-    "/admin/attend": Admin_Attend,
-    "/admin/commute": Admin_Commute,
-    "/admin/notice": Admin_Notice,
-    "/admin/user-list": Admin_UserList,
+const routes = {
+  //common
+  "/": Home,
+  "/login": Login,
 
-    "/user/attend": User_Attend,
-    "/user/my-page": User_MyPage,
+  //admin
+  "/admin/attend": Admin_Attend,
+  "/admin/commute": Admin_Commute,
+  "/admin/notice": Admin_Notice,
+  "/admin/user-list": Admin_UserList,
 
-    "/login": Login,
-  };
+  //user
+  "/user/attend": User_Attend,
+  "/user/my-page": User_MyPage,
+};
+
+async function router() {
 
   const path = window.location.pathname;
   const render = routes[path] ?? NotFound;
 
-  document.getElementById("root").innerHTML = path !== '/login' ? Layout(render()) : render();
+  document.getElementById("root").innerHTML = path !== '/login' ? Layout( await render()) : render();
 }
 
 document.addEventListener("DOMContentLoaded", app);
--- src/pages/admin/commute/commute-page.js
+++ src/pages/admin/commute/commute-page.js
@@ -1,3 +1,62 @@
-const Commute = () => `<h1>Commute page</div>`;
+import { fetchCommutes } from "../../../api/commuteApi";
+import { delete_icon, edit_icon, search_icon } from "../../../icons";
+import styles from "./commute.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+const Commute = async () => {
+
+  return `
+  <section class="${cx("container")}">
+    <header class="${cx("header")}">
+      <div class="${cx("header-title")}">
+        <h1>출퇴근 관리(admin)</h1>
+        <div class="${cx("search")}">
+          <input type="text" placeholder="사원명" />
+          ${search_icon}
+        </div>
+      </div>
+      <select>
+        <option value="all">모두 보기</option>
+        <option value="present">정상</option>
+        <option value="absent">결근</option>
+        <option value="late">지각</option>
+        <option value="early-leave">조퇴</option>
+      </select>
+    </header>
+    <div class="${cx("requests")}">
+      <div class="${cx("grid", "request-header")}">
+        <div>상태</div>
+        <div>사원명</div>
+        <div>일시</div>
+        <div>출근 시간</div>
+        <div>퇴근 시간</div>
+        <div>수정 / 삭제</div>
+      </div>
+    ${await list_items()}    
+`;
+};
+
+const list_items = async () => {
+  const res = await fetchCommutes();
+  return res
+    .map(
+      ({ userId, date, arriveTime, leaveTime }) => `
+    <div class="${cx("grid", "request-item")}">
+      <div>정상</div>
+      <div>${userId}</div>
+      <div>${date}</div>
+      <div>${arriveTime}</div>
+      <div>${leaveTime}</div>
+      <div class="${cx("request-tools")}">
+        ${edit_icon}
+        ${delete_icon}
+      </div>
+    </div>
+  `
+    )
+    .join("");
+};
 
 export default Commute;
