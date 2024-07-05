94a275b590a30cc65f14e49d249054df02de699c
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-07-05 13:43:02 +0900

    필터, 검색 기능 추가

---
+++ src/pages/admin/commute/render-list-item.js
@@ -0,0 +1,50 @@
+import classNames from "classnames/bind";
+import { fetchCommutes } from "../../../api/commuteApi";
+import { delete_icon, edit_icon } from "../../../assets/icons";
+import styles from "./commute.module.css";
+import setCommuteStatus from "./setCommuteStatus";
+
+const cx = classNames.bind(styles);
+
+const render_list_items = async (param = "all", from = "select") => {
+  if (window.commuteData.length == 0) {
+    window.commuteData = await fetchCommutes();
+    window.commuteData = window.commuteData.map((data) => ({
+      ...data,
+      status: setCommuteStatus(data.arriveTime, data.leaveTime),
+    }));
+  }
+
+  const container = document.querySelector(`.${cx("request-item-container")}`);
+
+  const filterd_list =
+    from == "select"
+      ? window.commuteData.filter((data) =>
+          param == "all" ? true : data.status === param
+        )
+      : window.commuteData.filter((data) =>
+          Object.values(data).some((x) => x == param)
+        );
+
+  const list_item = filterd_list
+    .map(
+      ({ status, userId, date, arriveTime, leaveTime }) => `
+    <div class="${cx("grid", "request-item")}">
+      <div>${status}</div>
+      <div>${userId}</div>
+      <div>${date}</div>
+      <div>${arriveTime}</div>
+      <div>${leaveTime}</div>
+      <div class="${cx("request-tools")}">
+        <img src=${edit_icon} class="${cx("icon")}" alt="edit-icon">
+        <img src=${delete_icon} class="${cx("icon")}" alt="delete-icon">
+      </div>
+    </div>
+  `
+    )
+    .join("");
+  if (list_item.length == 0) return (container.innerHTML = "NO DATA");
+  container.innerHTML = list_item;
+};
+
+export default render_list_items;
