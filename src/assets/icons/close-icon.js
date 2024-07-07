ea70be7eabcbb39626e0675282ec36778393b604
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-07-07 17:41:25 +0900

    js형태 아이콘 추가, color constant 파일 추가

---
+++ src/assets/icons/close-icon.js
@@ -0,0 +1,15 @@
+import { white } from "../../constants/colors";
+
+const close_icon = (color = white) => `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="24px"
+  viewBox="0 -960 960 960"
+  width="24px"
+  fill="${color}"
+>
+  <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+</svg>
+`;
+
+export default close_icon
