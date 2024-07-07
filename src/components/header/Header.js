9fb8bdb2ddaea742f39e9d46d7610d63cb8796e4
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-07-07 17:59:48 +0900

    헤더 메뉴 로직 수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,20 +6,22 @@ const cx = classNames.bind(styles);
 const placeholder = "https://via.placeholder.com/100";
 
 
-function userIsAdmin() {
+function isAdmin() { // 관리자 여부 확인
+  const path = window.location.pathname;
+  console.log(path)
   // 실제 사용자 인증 로직으로 교체
-  return true;
+  return path.includes('/admin')
 }
 
 const Header = () => {
-  const isAdmin = userIsAdmin(); // 관리자 여부 확인
+  console.log(isAdmin())
 
   // 관리자용 메뉴 항목
   const adminMenu = `
     <a href="/admin/notice">공지사항</a>
     <a href="/admin/user-list">직원정보</a>
     <a href="/admin/attend">근태관리</a>
-    <a href="/user/attend">근태현황</a>
+    <a href="/admin/commute">출퇴근관리</a>
   `;
 
   // 일반 사용자용 메뉴 항목
